43fef474f43c0b142d0ecbfb9f02dcaa445d3950
Brian Denmark <testingarea479@gmail.com>
2022-08-04 10:33:51 -0700

    completed tasks 1 and 2.

--- file.py
+++ file.py
@@ -1,1 +1,21 @@
 # Here we go! We got this.😎
+# 1. Update Values in Dictionaries and Lists
+    # 1. Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ] 
+x = [ [5,2,3], [10,8,9] ] 
+x[1][0] = 15
+print(x)
+    # 2. Change the last_name of the first student from 'Jordan' to 'Bryant'
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
