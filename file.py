4d6ffc3a8372611dd232f46549e1f3c30c362657
Brian Denmark <testingarea479@gmail.com>
2022-08-04 10:49:25 -0700

    saving dictionary iteration problem progress, will update soon for solution

--- file.py
+++ file.py
@@ -22,8 +22,32 @@ sports_directory = {
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory)
 
-z = [ {'x': 10, 'y': 20} ]
     # 4. Change the value 20 in z to 30
+z = [ {'x': 10, 'y': 20} ]
+
 z[0]['y'] = 30
 print(z)
 
+# 2. Iterate Through a List of Dictionaries
+    # Create a function iterateDictionary(some_list) that, given a list of dictionaries, 
+    # the function loops through each dictionary in the list and prints each key and the 
+    # associated value. For example, given the following list:
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(list):
+    for i in list:
+        print(list[['first_name'][i]['last_name'][i]])
+
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
